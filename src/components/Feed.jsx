fb426ccc8d39bd51a9fa7334058a1793dda7af1c
a0m0snj <ayush.maheshwari@walmart.com>
2023-01-01 17:07:41 +0530

    first commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { client } from "../sanity-client";
+import { searchQuery, feedQuery } from "../utils/sanity-data";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
+
+const Feed = () => {
+  const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState(null);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+
+  if (loading) return <Spinner message="We are loading your feed!" />;
+  if (!pins?.length) return <h2>No pins available!</h2>;
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
+};
+
+export default Feed;
---
+++ src/components/Search.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+
+import { client } from "../sanity-client";
+import { feedQuery, searchQuery } from "../utils/sanity-data";
+import Spinner from "./Spinner";
+import MasonryLayout from "./MasonryLayout";
+
+const Search = ({ searchTerm }) => {
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchTerm !== "") {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+
+  return (
+    <div>
+      {loading && <Spinner message="Searching pins" />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== "" && !loading && (
+        <div className="mt-10 text-center text-xl ">No Pins Found!</div>
+      )}
+    </div>
+  );
+};
+
+export default Search;
